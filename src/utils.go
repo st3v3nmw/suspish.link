9b06020ff3a9b76e549c83ec522738bbf0d036c4
Stephen Mwangi <mail@stephenmwangi.com>
2023-11-11 19:01:26 +0300

    Fix HTTP schemes behind proxy servers

--- src/utils.go
+++ src/utils.go
@@ -8,9 +8,12 @@ import (
 )
 
 func GetHttpScheme(c *gin.Context) string {
-	scheme := "http"
-	if c.Request.TLS != nil {
-		scheme = "https"
+	scheme := c.Request.Header.Get("X-Forwarded-Proto")
+	if scheme == "" {
+		scheme = "http"
+		if c.Request.TLS != nil {
+			scheme = "https"
+		}
 	}
 	return scheme
 }
